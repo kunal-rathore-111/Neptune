09da13ebf805365c75f8200f1609039ba869668f
Kunal-Rathore <kunalworkspace111@gmail.com>
2026-05-24 07:24:03 +0530

    fix: add cors to ai server

--- apps/aiServer/src/index.ts
+++ apps/aiServer/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
 import 'dotenv/config';
 import { secureHeaders } from 'hono/secure-headers';
@@ -10,7 +11,34 @@ const app = new Hono(); // same as express just faster
 
 app.use('*', secureHeaders()); //same as helmet in express
 
+const origins = [process.env.Frontend_URL, 'http://localhost:5173'].filter((origin): origin is string =>
+  Boolean(origin),
+);
 
+app.use('*', async (c, next) => {
+  const origin = c.req.header('origin');
+
+  if (origin && !origins.includes(origin)) {
+    return c.json({ message: 'Invalid Origin' }, 500);
+  }
+
+  await next();
+});
+
+app.use(
+  '*',
+  cors({
+    origin: (origin) => {
+      if (!origin) return '';
+      if (origins.includes(origin)) return origin;
+      return '';
+    },
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    maxAge: 24 * 60 * 60,
+    allowHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
 
 app.get('/', (c) => {
   return c.json(
