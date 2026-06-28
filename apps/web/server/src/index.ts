9372318dd99ca8b5588f28fe701de7e0b4488cb8
Kunal-Rathore <kunalworkspace111@gmail.com>
2026-06-28 11:01:25 +0530

    fix: resolve Vercel default export bug

--- apps/web/server/src/index.ts
+++ apps/web/server/src/index.ts
@@ -14,7 +14,7 @@ import AppError from './middlewares/appError';
 import { NODE_ENV } from './libs/utils/envVariables';
 import { OAuthMiddleware } from './middlewares/OAuthMiddleware';
 
-const app = express();
+export const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 app.use(helmet());
